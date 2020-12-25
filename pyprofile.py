6e7c40e2e27e259c7e984df2db1ac1989b639390
rrigato <ryanrigato@gmail.com>
2020-12-25 07:49:15 -0800

    example python project setup

---
+++ pyprofile.py
@@ -0,0 +1,13 @@
+from util.submodule import stub_routine_submod
+
+def sub_routine_pyprofile():
+    stub_routine_submod()
+    stub_routine_submod()
+
+def main():
+    """Calls sub_routine_pyprofile
+    """
+    sub_routine_pyprofile()
+
+if __name__ == "__main__":
+    main()
