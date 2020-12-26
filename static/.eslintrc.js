17d3d124b0fba3417dc9c16ce3ff0284ebfb5113
rrigato <ryanrigato@gmail.com>
2020-12-26 13:13:16 -0800

    basic eslint config

--- static/.eslintrc.js
+++ static/.eslintrc.js
@@ -3,11 +3,15 @@ module.exports = {
         "browser": true,
         "es2020": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "airbnb"
+    ],
     "parserOptions": {
         "ecmaVersion": 12,
         "sourceType": "module"
     },
     "rules": {
+        "linebreak-style": ["error", "windows"]
     }
 };
