cdb82a8a6a07816bd874085c8c094d7f4b5fb526
rrigato <ryanrigato@gmail.com>
2020-12-30 08:57:48 -0800

    weblogin common js

--- static/web_login.js
+++ static/web_login.js
@@ -1,11 +1,14 @@
-import "regenerator-runtime";
-import puppeteer from 'puppeteer';
- 
+// import "regenerator-runtime";
+// import puppeteer from 'puppeteer';
+
+const puppeteer = require("puppeteer");
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch(headless=false);
   const page = await browser.newPage();
   await page.goto('https://reddit.com');
  
+  //process.env.REDDIT_USERNAME
+  //process.env.REDDIT_PASSWORD
   // Get the "viewport" of the page, as reported by the page.
   const dimensions = await page.evaluate(() => {
     return {
